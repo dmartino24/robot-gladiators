6480dbbf81e042d94d2d06473b5b786fe21efe2a
Dustin Martin <d_man_24_3@hotmail.com>
2022-05-19 01:59:22 -0500

    intro coded

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,32 @@
-window.alert("This an alert! Javascript is running!");
+var playerName = prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+console.log(playerName, playerAttack, playerHealth);
+
+var fight = function(){
+    alert("Welcome to Robot Gladiators!")
+}
+
+fight();
+
+//Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+enemyHealth -= playerAttack;
+// Log a resulting message to the console so we know that it worked.
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+// Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+playerHealth -= enemyAttack;
+// Log a resulting message to the console so we know that it worked.
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+// check enemy's health
+if (enemyHealth <= 0) {
+    alert(enemyName + " has died!");
+  } 
+  else {
+    alert(enemyName + " still has " + enemyHealth + " health left.");
+}
